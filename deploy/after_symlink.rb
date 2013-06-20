9ad5196c4db3d94dc01df573db155b8989e5e5e7
Jim Neath <jimneath@googlemail.com>
2013-06-20 13:43:45 +0100

    Added deploy hooks

--- deploy/after_symlink.rb
+++ deploy/after_symlink.rb
@@ -1,4 +1,4 @@
-on_app_and_utilities do
+on_app_servers_and_utilities do
   run "[[ -d #{shared_path}/sphinx ]] && ln -nfs #{shared_path/sphinx} #{current_release}/db/sphinx"
   run "cd #{current_release} && RAILS_ENV=#{environment} bundle exec rake ts:configure"
 end
