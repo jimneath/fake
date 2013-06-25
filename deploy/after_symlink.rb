3bd7ee90aa5d85a26209a1ec77dd7f682b35c815
Jim Neath <jimneath@googlemail.com>
2013-06-25 11:24:00 +0100

    tweaks

--- deploy/after_symlink.rb
+++ deploy/after_symlink.rb
@@ -1,4 +1,4 @@
 on_app_servers_and_utilities do
-  run "[[ -d #{shared_path}/sphinx ]] && ln -nfs #{shared_path}/sphinx #{current_release}/db/sphinx"
-  run "cd #{current_release} && RAILS_ENV=#{environment} bundle exec rake ts:configure"
+  run "[[ -d #{shared_path}/sphinx ]] && ln -nfs #{shared_path}/sphinx #{current_path}/db/sphinx"
+  run "cd #{current_path} && RAILS_ENV=#{environment} bundle exec rake ts:configure"
 end
