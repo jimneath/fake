70e8e81ed10a28c6d0756402d30ec2628aa60e1e
Jim Neath <jimneath@googlemail.com>
2013-07-09 09:31:10 +0100

    rails 4

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Fake::Application.config.secret_token = '40955b16945e038c732f45f7b6dd60ae102f51e0119a7c0180ea3936e77654902e7e0a745df820435f745bdbc6123dea8ea70a3958212d3f7bbf0bfa91c584ee'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fake::Application.config.secret_key_base = '22c6d1abc8d2fa2ecb4b310289223529fa7ac30b891815454b2305fc9d2b9d4dee68ecbee04b0f36a3cacb22aa02bd3cc67d66c3aa25251fc18ad0ad99a0c876'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,7 @@
-500.times do
-  Person.create do |p|
-    p.first_name = Faker::Name.first_name
-    p.last_name = Faker::Name.last_name
-    p.age = rand(18..70)
-    p.location = Faker::AddressUK.county
-    p.married = [true, false].sample
-  end
-end
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
